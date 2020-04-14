59f4a2dfb20c877b90ad4a2f99a82b72e3eefaa8
10672661 <10672661@qq.com>
2020-04-14 14:08:25 +0800

    add ;

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("aaronlinv");
-	System.out.println("10672661")
+	System.out.println("10672661");
     }
 }
